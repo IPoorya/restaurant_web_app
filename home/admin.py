29fadbaef43609dc013a3defcf89172695bbda8e
IPoorya <brabbitcontact@gmail.com>
2023-08-09 16:11:39 +0330

    category and food model added + admin panel

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Category, Food
+from django.urls import reverse
+from django.utils.html import format_html
 
-# Register your models here.
+
+class FoodInline(admin.StackedInline):
+    model = Food
+    readonly_fields = ['sells']
+    extra = 0
+
+
+@admin.register(Food)
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ['name', 'display_category', 'price',
+                    'sells', 'created', 'available']
+    readonly_fields = ['sells']
+    search_fields = ['name']
+
+    def display_category(self, obj):
+        url = reverse("admin:home_category_change", args=[obj.category.id])
+        print(url)
+        return format_html('<a href="{}">{}</a>', url, obj.category.name)
+    display_category.short_description = 'category'
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'foods_number']
+    readonly_fields = ['foods_number']
+    search_fields = ['name']
+    inlines = [
+        FoodInline,
+    ]
--- home/models.py
+++ home/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    foods_number = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Food(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='food_images/')
+    description = models.TextField()
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name='foods')
+    price = models.PositiveIntegerField()
+    available = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now=True)
+    sells = models.PositiveBigIntegerField(default=0)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.category.foods_number += 1
+        self.category.save()
+        return super().save(*args, **kwargs)
